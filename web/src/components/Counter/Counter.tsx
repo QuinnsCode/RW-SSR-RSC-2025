7d08e89031b897a78e67010f543993a177106970
Rob Cameron <cannikin@fastmail.com>
2024-05-30 13:54:41 -0700

    Adds delay() function, using it in HomePage and Counter

--- web/src/components/Counter/Counter.tsx
+++ web/src/components/Counter/Counter.tsx
@@ -2,26 +2,20 @@
 
 import React from 'react'
 
-import { Metadata } from '@redwoodjs/web'
+import { delay } from 'src/lib/delay'
 
 // @ts-expect-error no types
 import styles from './Counter.module.css'
+
 import './Counter.css'
 
-export const Counter = () => {
+export const Counter = async () => {
   const [count, setCount] = React.useState(0)
+
+  await delay(1000)
 
   return (
     <>
-      <Metadata
-        title="RSC + SSR Demo"
-        description="Demonstrates React Server Components and Server Side Rendering in RedwoodJS"
-        og={{
-          image:
-            'https://maxm-imggenurl.web.val.run/a-forest-of-redwood-trees.png',
-        }}
-      />
-
       <div style={{ border: '3px blue dashed', margin: '1em', padding: '1em' }}>
         <p>Count: {count}</p>
         <button onClick={() => setCount((c) => c + 1)}>Increment</button>
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,31 @@
 import { Counter } from 'src/components/Counter'
+import { delay } from 'src/lib/delay'
 
 // @ts-expect-error no types
 import styles from './HomePage.module.css'
 
 import './HomePage.css'
 
-const HomePage = ({ name = 'Anonymous' }) => {
+const HomePage = async ({ name = 'Anonymous' }) => {
+  await delay(1000)
+
   return (
     <>
+      <meta name="title" content="RSC + SSR Demo" />
+      <meta
+        name="description"
+        content="Demonstrates React Server Components and Server Side Rendering in RedwoodJS"
+      />
+      <meta property="og:title" content="RSC + SSR Demo" />
+      <meta
+        property="og:description"
+        content="Demonstrates React Server Components and Server Side Rendering in RedwoodJS"
+      />
+      <meta
+        property="og:image"
+        content="https://maxm-imggenurl.web.val.run/a-forest-of-redwood-trees.png"
+      />
+
       <div className="home-page">
         <div
           style={{ border: '3px red dashed', margin: '1em', padding: '1em' }}
